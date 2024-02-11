65678bc1668bc9d7dad73272dc9dc582be5b9f37
mgfahem <mgfahem@gmail.com>
2024-02-11 19:11:33 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ListPlaylist from "./component/ListPlaylist.tsx";
+
+function App() {
+
+    return (
+        <>
+            <ListPlaylist/>
+        </>
+    )
+}
+
+export default App;
